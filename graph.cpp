56f1f6430cae8cddd428fe5e1484c20df872cfc4
Ben Fidlers <ben.fidlers@qmino.com>
2024-02-29 15:54:39 +0100

    add empty constructor face struct

--- graph.cpp
+++ graph.cpp
@@ -4,6 +4,9 @@
 struct Face {
   std::string id;
 
+  Face() {
+    id = "blank";
+  }
   Face(std::string s) {
     id = s;
   }
@@ -15,7 +18,9 @@ std::ostream& operator <<(std::ostream& os, const Face& f) {
 
 void load_graph() {
   Face f("test");
+  Face f2;
   std::cout << f << std::endl;
+  std::cout << f2 << std::endl;
 
   std::cout << "Graph loaded!" << std::endl;
 }
