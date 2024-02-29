fd2fe13c231effa6341c03dbbc0ac3def4ca3427
Ben Fidlers <ben.fidlers@qmino.com>
2024-02-29 15:50:35 +0100

    add face structure

--- graph.cpp
+++ graph.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+#include <string>
+
+struct Face {
+  std::string id;
+
+  Face(std::string s) {
+    id = s;
+  }
+};
+
+std::ostream& operator <<(std::ostream& os, const Face& f) {
+  return os << "Face with id: " << f.id << std::endl;
+}
 
 void load_graph() {
+  Face f("test");
+  std::cout << f << std::endl;
+
   std::cout << "Graph loaded!" << std::endl;
 }
