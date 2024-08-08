7acd3548d290d0d6b48a6df01555ab83c2a6189b
Ben Fidlers <ben.fidlers@qmino.com>
2024-08-08 11:28:24 +0200

    move project to subfolder

---
+++ cpp/build/CMakeFiles/ProcMod_lib.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/benfidlers/Documents/personal/thesis/code/procedural-modeling/CameraTrackball.cpp" "CMakeFiles/ProcMod_lib.dir/CameraTrackball.cpp.o" "gcc" "CMakeFiles/ProcMod_lib.dir/CameraTrackball.cpp.o.d"
+  "/Users/benfidlers/Documents/personal/thesis/code/procedural-modeling/Edge.cpp" "CMakeFiles/ProcMod_lib.dir/Edge.cpp.o" "gcc" "CMakeFiles/ProcMod_lib.dir/Edge.cpp.o.d"
+  "/Users/benfidlers/Documents/personal/thesis/code/procedural-modeling/EdgeLabel.cpp" "CMakeFiles/ProcMod_lib.dir/EdgeLabel.cpp.o" "gcc" "CMakeFiles/ProcMod_lib.dir/EdgeLabel.cpp.o.d"
+  "/Users/benfidlers/Documents/personal/thesis/code/procedural-modeling/Face.cpp" "CMakeFiles/ProcMod_lib.dir/Face.cpp.o" "gcc" "CMakeFiles/ProcMod_lib.dir/Face.cpp.o.d"
+  "/Users/benfidlers/Documents/personal/thesis/code/procedural-modeling/Graph.cpp" "CMakeFiles/ProcMod_lib.dir/Graph.cpp.o" "gcc" "CMakeFiles/ProcMod_lib.dir/Graph.cpp.o.d"
+  "/Users/benfidlers/Documents/personal/thesis/code/procedural-modeling/GraphBoundary.cpp" "CMakeFiles/ProcMod_lib.dir/GraphBoundary.cpp.o" "gcc" "CMakeFiles/ProcMod_lib.dir/GraphBoundary.cpp.o.d"
+  "/Users/benfidlers/Documents/personal/thesis/code/procedural-modeling/Input.cpp" "CMakeFiles/ProcMod_lib.dir/Input.cpp.o" "gcc" "CMakeFiles/ProcMod_lib.dir/Input.cpp.o.d"
+  "/Users/benfidlers/Documents/personal/thesis/code/procedural-modeling/Point.cpp" "CMakeFiles/ProcMod_lib.dir/Point.cpp.o" "gcc" "CMakeFiles/ProcMod_lib.dir/Point.cpp.o.d"
+  "/Users/benfidlers/Documents/personal/thesis/code/procedural-modeling/Primitive.cpp" "CMakeFiles/ProcMod_lib.dir/Primitive.cpp.o" "gcc" "CMakeFiles/ProcMod_lib.dir/Primitive.cpp.o.d"
+  "/Users/benfidlers/Documents/personal/thesis/code/procedural-modeling/Vertex.cpp" "CMakeFiles/ProcMod_lib.dir/Vertex.cpp.o" "gcc" "CMakeFiles/ProcMod_lib.dir/Vertex.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cpp/build/CMakeFiles/ProcMod_lib.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProcMod_lib.dir/CameraTrackball.cpp.o"
+  "CMakeFiles/ProcMod_lib.dir/CameraTrackball.cpp.o.d"
+  "CMakeFiles/ProcMod_lib.dir/Edge.cpp.o"
+  "CMakeFiles/ProcMod_lib.dir/Edge.cpp.o.d"
+  "CMakeFiles/ProcMod_lib.dir/EdgeLabel.cpp.o"
+  "CMakeFiles/ProcMod_lib.dir/EdgeLabel.cpp.o.d"
+  "CMakeFiles/ProcMod_lib.dir/Face.cpp.o"
+  "CMakeFiles/ProcMod_lib.dir/Face.cpp.o.d"
+  "CMakeFiles/ProcMod_lib.dir/Graph.cpp.o"
+  "CMakeFiles/ProcMod_lib.dir/Graph.cpp.o.d"
+  "CMakeFiles/ProcMod_lib.dir/GraphBoundary.cpp.o"
+  "CMakeFiles/ProcMod_lib.dir/GraphBoundary.cpp.o.d"
+  "CMakeFiles/ProcMod_lib.dir/Input.cpp.o"
+  "CMakeFiles/ProcMod_lib.dir/Input.cpp.o.d"
+  "CMakeFiles/ProcMod_lib.dir/Point.cpp.o"
+  "CMakeFiles/ProcMod_lib.dir/Point.cpp.o.d"
+  "CMakeFiles/ProcMod_lib.dir/Primitive.cpp.o"
+  "CMakeFiles/ProcMod_lib.dir/Primitive.cpp.o.d"
+  "CMakeFiles/ProcMod_lib.dir/Vertex.cpp.o"
+  "CMakeFiles/ProcMod_lib.dir/Vertex.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProcMod_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
