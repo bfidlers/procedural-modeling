7cd3f64ffd30837ad5f64ed78ec873fb8885dc28
Ben Fidlers <ben.fidlers@qmino.com>
2024-08-08 11:53:15 +0200

    hello world pyglet

--- python/main.py
+++ python/main.py
@@ -1,6 +1,26 @@
-def print_hi(name):
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+import pyglet
 
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
+window = pyglet.window.Window()
+
+label = pyglet.text.Label('Hello, world',
+                          font_name='Times New Roman',
+                          font_size=36,
+                          x=window.width//2, y=window.height//2,
+                          anchor_x='center', anchor_y='center')
+
+
+@window.event
+def on_key_press(symbol, modifiers):
+    print('A key was pressed')
+    print(symbol)
+    print(modifiers)
+
+
+@window.event
+def on_draw():
+    window.clear()
+    label.draw()
+
+
+pyglet.app.run()
