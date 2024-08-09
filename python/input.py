b12ee4249105e66bf80532e45ebb05c87ed47ad7
Ben Fidlers <ben.fidlers@qmino.com>
2024-08-09 23:21:20 +0200

    extend input examples

--- python/input.py
+++ python/input.py
@@ -4,7 +4,8 @@ from graph import Graph
 
 def load_graph():
     # return load_triangle()
-    return load_square()
+    # return load_square()
+    return load_cube()
 
 
 def load_triangle():
@@ -23,16 +24,40 @@ def load_triangle():
 
 def load_square():
     g = Graph()
+    add_square(g, 1, 0, 0)
+    return g
 
-    g.add_vertex(1, Point(-0.5, -0.5))
-    g.add_vertex(2, Point(-0.5, 0.5))
-    g.add_vertex(3, Point(0.5, 0.5))
-    g.add_vertex(4, Point(0.5, -0.5))
 
-    g.add_edge("a", 1, 2)
-    g.add_edge("b", 2, 3)
-    g.add_edge("c", 4, 3)
-    g.add_edge("d", 1, 4)
+def load_cube():
+    g = Graph()
+    add_square(g,1, 0, 0, -0.5)
+    add_square(g,1, 0, 0, 0.5)
+
+    e = g.edge_size()
+
+    g.add_edge(str(e), 0, 4)
+    g.add_edge(str(e + 1), 1, 5)
+    g.add_edge(str(e + 2), 2, 6)
+    g.add_edge(str(e + 3), 3, 7)
 
     return g
 
+
+def add_square(g, length, x, y, z=0):
+    base_x = x - length / 2
+    base_y = y - length / 2
+
+    v = g.vertex_size()
+
+    g.add_vertex(v, Point(base_x, base_y, z))
+    g.add_vertex(v + 1, Point(base_x + length, base_y, z))
+    g.add_vertex(v + 2, Point(base_x + length, base_y + length, z))
+    g.add_vertex(v + 3, Point(base_x, base_y + length, z))
+
+    e = g.edge_size()
+
+    # TODO should ids be generated?
+    g.add_edge(str(e), v, v + 1)
+    g.add_edge(str(e + 1), v + 1, v + 2)
+    g.add_edge(str(e + 2), v + 2, v + 3)
+    g.add_edge(str(e + 3), v + 3, v)
